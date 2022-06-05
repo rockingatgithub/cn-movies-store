477b2720ea20b01a9fe3ed7a97f35260a481cfaf
rockingatgithub <contactwithsid@gmail.com>
2022-06-05 19:22:23 +0530

    initialized react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const API_KEY = 'e08d15260b80f4fba575a381012e7ce8';
+
+// const BASE_URL = 'https://api.themoviedb.org/3';
+
+class App extends React.Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+
+      moviesList: [],
+      favouriteList: [],
+
+    }
+
+  }
+
+  componentDidMount = async () => {
+
+    let response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e08d15260b80f4fba575a381012e7ce8&language=en-US&page=1`)
+
+    let parsedResponse = await response.json();
+
+    this.setState({
+      moviesList: [ ...parsedResponse.results  ]
+    })
+
+    
+  }
+
+  addToFavourite = (movie) => {
+
+   this.setState((prevState) => ({ favouriteList: [...prevState.favouriteList, movie]  }) )
+
+  }
+
+  render () {
+
+    const { moviesList  } = this.state;
+
+    return (
+
+      <div>
+
+          <h3>Welcome to the Movies Store</h3>
+
+          <div className="movies-container" >
+
+            <ul className="movies-list">
+              {moviesList.map((movie) => 
+
+
+                <li>
+                  <i class="fa fa-heart" aria-hidden="true" onClick={() => this.addToFavourite(movie)} ></i>
+                  <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="movie-logo"  />
+
+                  <span>
+                    <span>{movie.title}</span>
+                    <br/>
+                    <span>{ movie.release_date }</span>
+                    <progress value={movie.vote_average} max={10} color="blue" />      
+                  </span>                    
+                </li>
+
+              )}
+            </ul>
+
+          </div>
+
+      </div>
+
+    )
+
+  }
+
 }
+
 
 export default App;
